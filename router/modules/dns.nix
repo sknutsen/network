a53d401f04c36ba251f45eed87ee1ecd87f93340
sknutsen <sondreknutsen1@gmail.com>
2026-09-07 17:25:00 +0200

    grafana and capacitor dns

--- router/modules/dns.nix
+++ router/modules/dns.nix
@@ -86,6 +86,8 @@ in
           ''"immich.${C.domain}. A ${C.hosts.caddy}"''
           ''"auth.${C.domain}. A ${C.hosts.caddy}"''
           ''"code.${C.domain}. A ${C.hosts.caddy}"''
+          ''"grafana.${C.domain}. A ${C.hosts.caddy}"''
+          ''"capacitor.${C.domain}. A ${C.hosts.caddy}"''
           # Exact names only: local-zone type static does not synthesize
           # answers from *.lab.zdk.no (foo.lab / grafana.lab NXDOMAIN).
           ''"${C.domain}. A ${C.hosts.caddy}"''
