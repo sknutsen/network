4c9ac244b1458adfdc8315ee9619a06b704a1221
sknutsen <sondreknutsen1@gmail.com>
2026-09-07 16:00:19 +0200

    nordri enable k3s

--- nodes/hosts/nordri.nix
+++ nodes/hosts/nordri.nix
@@ -10,7 +10,7 @@
     diskLayout = "giyomoon-image";
     diskDevice = "/dev/disk/by-id/nvme-KINGSTON_SNV2S500G_50026B76866B3AE3";
     enableIpv6 = true; # lab ULA; no WAN v6
-    enableK3s = false; # Stage 5
+    enableK3s = true; # Stage 5
     enableLonghornPrep = true;
     k3sTokenFile = null;
   };
